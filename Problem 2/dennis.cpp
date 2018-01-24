0ca52267605d31e778af84ab6d8f9938a178e37f
Dennis Pham <dennispham97@hotmail.com>
2018-01-24 13:48:11 -0600

    optimized solution code.

--- Problem 2/dennis.cpp
+++ Problem 2/dennis.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int main()
 {
-    long num1 = 0, num2 = 1, print = 0;
-    long temp = 0;
-    for(int i = 0; temp < 4000001; i++)
+    int num1 = 0, num2 = 1, temp = 0, print = 0;
+    
+    while(temp < 4000001)
     {
         temp = num1 + num2;
         num1 = num2;
         num2 = temp;
         if(num2 % 2 == 0)
             print += num2;
-        cout << "first number " << num1 << " second number " << num2 << endl;
     }
     
     cout << print << endl;
     return 0;
 }
+
+
+//answer is 4613732
